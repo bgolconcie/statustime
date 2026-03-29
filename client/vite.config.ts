a7995440e563f815fad0179da866cbb22bd51ea5
bgolconcie <bgol@concie.co>
2026-03-29 10:29:22 +0300

    update client/vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:3000' } },
-  build: { outDir: '../public', emptyOutDir: true }
+  build: {
+    outDir: '../public',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
